2d597d7aec0c7be77c6b8de08c9b826822660d98
Cargill Seiveright <microcargill@hotmail.com>
2020-03-01 17:32:38 -0500

    Added login and secure page

--- app/views.py
+++ app/views.py
@@ -11,6 +11,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
 import os
+from werkzeug.security import check_password_hash
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
 
@@ -35,24 +36,50 @@ def about():
 def login():
     form = LoginForm()
     if request.method == "POST":
-        # change this to actually validate the entire form submission
-        # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
-
+            username = form.username.data
+            password = form.password.data
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
+            user = UserProfile.query.filter_by(username=username).first()
 
-            # get user id, load into session
-            login_user(user)
+            if user is not None and check_password_hash(user.password, password):
+                
+                # get user id, load into session
+                login_user(user)
 
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+                # remember to flash a message to the user
+                flash('Logged in successfully.', 'success')
+                return redirect(url_for('secure_page'))  # they should be redirected to a secure-page route instead
+            else:
+                flash('Username or Password is incorrect.', 'danger')
+
+    flash_errors(form)
     return render_template("login.html", form=form)
 
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    return render_template('secure_page.html')
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ), 'danger')
+
+@app.route('/testing')
+def testing():
+    #user = UserProfile(first_name="Cargill", last_name="Seiveright", username="sivrite", password="password")
+    #db.session.add(user)
+    #db.session.commit()
+    return "done"
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
